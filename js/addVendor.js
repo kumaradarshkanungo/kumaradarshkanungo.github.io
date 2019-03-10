1118e12743ffb63a65d04e0ea37ec016efc3cd7c
Kumar Adarsh Kanungo <kanungo.kumaradarsh@gmail.com>
2019-03-10 18:11:34 +0530

    driver completed

---
+++ js/addVendor.js
@@ -0,0 +1,21 @@
+// function fnHandlerStep1(){
+//     if(!$(".buttonNext").hasClass("buttonDisabled")){
+//         $(".buttonNext").addClass("buttonDisabled");
+//     }
+    
+//     if(!$("#name").val()){
+//         return;
+//     }
+//     if(!$("#number").val()){
+//         return;
+//     }
+//     if(!$("#owner-name").val()){
+//         return;
+//     }
+//     if(!$("#owner-number").val()){
+//         return;
+//     }
+//     $(".buttonNext").removeClass("buttonDisabled");
+// }
+
+
