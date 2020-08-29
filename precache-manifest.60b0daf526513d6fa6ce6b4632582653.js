33e5cc6afdc7fc4e4c01edfd90fc28ea2fb9a0a5
Kumar Adarsh Kanungo <kanungo.kumaradarsh@gmail.com>
2020-08-29 16:37:52 +0530

    Smart Dropdown

---
+++ precache-manifest.60b0daf526513d6fa6ce6b4632582653.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6451bac0d018022b7f1864944547a2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5426cc3fbe7b7d3be42d",
+    "url": "/static/css/2.52780007.chunk.css"
+  },
+  {
+    "revision": "0af98cffdbc2c4863363",
+    "url": "/static/css/main.2dcd8016.chunk.css"
+  },
+  {
+    "revision": "5426cc3fbe7b7d3be42d",
+    "url": "/static/js/2.fad1798b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.fad1798b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0af98cffdbc2c4863363",
+    "url": "/static/js/main.fb6375b8.chunk.js"
+  },
+  {
+    "revision": "226ff0b2a5d175968b84",
+    "url": "/static/js/runtime-main.4f1b1f38.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
